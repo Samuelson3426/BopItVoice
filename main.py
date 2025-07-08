143c22105c242a42774fe4cc7d7d6779750284d5
Sam Ridgers <samuelridgers@outlook.com>
2025-07-08 21:14:52 +0100

    Completely scrapped the 1-19 voices and re-wrote the entire system to work in place value grid

--- main.py
+++ main.py
@@ -2,36 +2,101 @@ from playsound3 import playsound
 
 import voices
 
-def check_number(numstr): # will return a list of voices to play to sound like the input number
-    voice_list = []
+def gen_one_digit(numstr, vl): # pretty simple - add plain number if not 0
+    if int(numstr[2]) != 0:
+        vl.append(voices.plain_numbers()[int(numstr[2])])
+    return vl
+
+def gen_ten_digit(numstr, vl): # digit 2 - gets complicated with teens here
+    if numstr[1] == '0':
+        return gen_one_digit(numstr, vl)
+
+    #TEEN LOGIC
+    if int(numstr[1]) == 1:
+        #10
+        if int(numstr[2]) == 0:
+            vl.append(voices.plain_numbers()[10])
+            return vl
+        #11
+        if int(numstr[2]) ==  1:
+            vl.append(voices.plain_numbers()[11])
+            return vl
+        #12
+        if int(numstr[2]) == 2:
+            vl.append(voices.plain_numbers()[12])
+            return vl
+        #13
+        if int(numstr[2]) == 3:
+            vl.append(voices.thir())
+            vl.append(voices.teen())
+            return vl
+        #14
+        if int(numstr[2]) == 5:
+            vl.append(voices.fif())
+            vl.append(voices.teen())
+            return vl
+        #ALL OTHER TEENS
+        vl.append(voices.plain_numbers()[int(numstr[2])])
+        vl.append(voices.teen())
+        return vl
     
-    if int(numstr) > 0 and int(numstr) <= 12:
-        voice_list.append(voices.plain_numbers()[int(numstr)])
-        return voice_list
+    #TWENTIES
+    if int(numstr[1]) == 2:
+        vl.append(voices.twen())
+        vl.append(voices.ty())
+        return gen_one_digit(numstr, vl)
     
-    if len(numstr) == 2 and int(numstr) > 12: # 2 digit numbers (>plain (which is >12))
+    #THIRTIES
+    if int(numstr[1]) == 3:
+        vl.append(voices.thir())
+        vl.append(voices.ty())
+        return gen_one_digit(numstr, vl)
+    
+    #FIFTIES
+    if int(numstr[1]) == 5:
+        vl.append(voices.fif())
+        vl.append(voices.ty())
+        return gen_one_digit(numstr, vl)
+    
+    vl.append(voices.plain_numbers()[int(numstr[1])])
+    vl.append(voices.ty())
+    return gen_one_digit(numstr, vl)
+
+def gen_hundred_digit(numstr, vl): # digit 3
+    if numstr[0] == '0':
+        return gen_ten_digit(numstr, vl)
+    
+    vl.append(voices.plain_numbers()[int(numstr[0])]) # simply add the voice of the digit in the hundreds column
+    vl.append(voices.hundred())
+    return gen_ten_digit(numstr, vl)
+
+def get_voice_list(numstr): # will return a list of voices to play to sound like the input number
+    vl = []
+    if len(numstr) <= 3:
+        voice_list = gen_hundred_digit(numstr, vl)
+    else:
+        voice_list = None
+
+    return voice_list
+
+def conv_str_int_to_three_letters(str_int):
+    new_str = ""
+    
+    if len(str_int) == 3:
+        return str_int
+    elif len(str_int) == 2:
+        new_str = '0'
+        new_str += str_int
+        return new_str
+    elif len(str_int) == 1:
+        new_str = '00'
+        new_str += str_int
+        return new_str
         
-        # TEEN NUMBERS
-        if int(numstr[0]) == 1:
-            
-            if int(numstr[1]) == 3: #13
-                voice_list.append(voices.thir())
-                voice_list.append(voices.teen())
-                return voice_list
-            
-            if int(numstr[1]) == 5: #15
-                voice_list.append(voices.fif())
-                voice_list.append(voices.teen())
-                return voice_list
-            
-            voice_list.append(voices.plain_numbers()[int(numstr[1])])
-            voice_list.append(voices.teen())
-            return voice_list
-            
             
 
 def bopit_say(numberstr):
-    voice_list = check_number(numberstr)
+    voice_list = get_voice_list(numberstr)
 
     if voice_list == None:
         voice_list = []
@@ -41,4 +106,4 @@ def bopit_say(numberstr):
         playsound(voice_list[i])
 
 while True:
-    bopit_say(input("BopIt Says: "))
+    bopit_say(conv_str_int_to_three_letters(input("BopIt Says: ")))
