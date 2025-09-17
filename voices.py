be9e244b17657f3b7044dab2cff059ad9bdceccb
Sam Ridgers <samuelridgers@outlook.com>
2025-09-17 10:03:19 +0100

    Modified constant path strigs to be CAPS

--- voices.py
+++ voices.py
@@ -1,44 +1,44 @@
 import os
 from random import randint
 
-voice_path = 'BopItExtreme2-Numbers'
-voice_path_error = 'BopItExtreme2-Error'
-voice_bop_it = 'BobItExtreme2-BopIt.wav'
+VOICE_PATH = 'BopItExtreme2-Numbers'
+VOICE_PATH_ERROR = 'BopItExtreme2-Error'
+VOICE_BOP_IT = 'BobItExtreme2-BopIt.wav'
 
 def bop_it():
-    return voice_bop_it
+    return VOICE_BOP_IT
 
 def plain_numbers():
     sdv = []
     for i in range(13): #first 12 numbers
         if i != 0:
-            sdv.append(os.path.join(voice_path, f'{i}.wav'))
+            sdv.append(os.path.join(VOICE_PATH, f'{i}.wav'))
         else:
             sdv.append("0.wav")
     return sdv
 
 def random_error():
     x = []
-    all_files = os.listdir(voice_path_error)
+    all_files = os.listdir(VOICE_PATH_ERROR)
     for i in range(len(all_files)):
         if all_files[i].endswith('.wav'):
-            x.append(os.path.join(voice_path_error, all_files[i]))
+            x.append(os.path.join(VOICE_PATH_ERROR, all_files[i]))
     return x[randint(0, len(x)-1)]
 
 def fif():
-    return os.path.join(voice_path, 'Fif-.wav')
+    return os.path.join(VOICE_PATH, 'Fif-.wav')
 
 def hundred():
-    return os.path.join(voice_path, 'Hundred.wav')
+    return os.path.join(VOICE_PATH, 'Hundred.wav')
 
 def thir():
-    return os.path.join(voice_path, 'Thir-.wav')
+    return os.path.join(VOICE_PATH, 'Thir-.wav')
 
 def twen():
-    return os.path.join(voice_path, 'Twen-.wav')
+    return os.path.join(VOICE_PATH, 'Twen-.wav')
 
 def teen():
-    return os.path.join(voice_path, '-teen.wav')
+    return os.path.join(VOICE_PATH, '-teen.wav')
 
 def ty():
-    return os.path.join(voice_path, '-ty.wav')
+    return os.path.join(VOICE_PATH, '-ty.wav')
